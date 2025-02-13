2ab4e1ee422ec2746b8ebdfe09d5ac6947dc104f
Ersajьn <75179594+yerssaintlaurent@users.noreply.github.com>
2025-02-14 04:17:08 +0500

    Update main.tsx

--- real-estate-search/src/main.tsx
+++ real-estate-search/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RealEstateSearch from "./RealEstateSearch.jsx";
+import "./index.css"; // Подключаем TailwindCSS
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RealEstateSearch />
+  </React.StrictMode>
+);
