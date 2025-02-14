99ca3a84831c9f45e09365901a415c332561df2c
Ersajьn <75179594+yerssaintlaurent@users.noreply.github.com>
2025-02-14 05:00:28 +0500

    Update main.tsx

--- real-estate-search/src/main.tsx
+++ real-estate-search/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import RealEstateSearch from "./RealEstateSearch";
 import "./index.css";
 
@@ -9,14 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RealEstateSearch />
   </React.StrictMode>
 );
-
-=======
-import RealEstateSearch from "./RealEstateSearch.jsx";
-import "./index.css"; // Подключаем TailwindCSS
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RealEstateSearch />
-  </React.StrictMode>
-);
->>>>>>> 2ab4e1ee422ec2746b8ebdfe09d5ac6947dc104f
